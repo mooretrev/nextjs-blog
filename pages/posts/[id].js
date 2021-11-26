af294f89ea1eca5303cb4397a1acf6dbac1b0a20
Trevor Moore <trevor.moore1999@gmail.com>
2021-11-26 00:38:31 -0600

    Lower case date

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import Layout from '../../components/layout'
-import Date from '../../components/Date'
+import Date from '../../components/date'
 import { getAllPostIds, getPostData } from '../../libs/posts'
 
 import utilStyles from '../../styles/utils.module.css'
